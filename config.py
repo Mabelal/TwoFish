5ec1661c4a66c3858fc0732a5ae309191f6c2f5c
Marc-Olivier Belanger <marc_olivier_29@hotmail.com>
2021-02-19 09:57:05 -0500

    Add config with db local file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '\x14B~^\x07\xe1\x197\xda\x18\xa6[[\x05\x03QVg\xce%\xb2<\x80\xa4\x00'
+    DEBUG = True
