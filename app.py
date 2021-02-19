c58d87fb302ce924cc18b4bf36ccaff3e633663b
Marc-Olivier Belanger <marc_olivier_29@hotmail.com>
2021-02-19 10:25:00 -0500

    Add migrations

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request
-# from models import db
+from config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '\x14B~^\x07\xe1\x197\xda\x18\xa6[[\x05\x03QVg\xce%\xb2<\x80\xa4\x00'
-app.config['DEBUG'] = True
+app.config.from_object(Config)
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG'] = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # db.init_app(app)
 
 # db.create_all()
--- models.py
+++ models.py
@@ -1,21 +1,21 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
+from app import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    todos = db.relationship('ToDO', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}'.format(self.username)
 
 
 class ToDO(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     body = db.Column(db.String(150))
-    due_date = db.Column(db.String(10))
+    due_date = db.Column(db.Date, index=True)
     is_complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
